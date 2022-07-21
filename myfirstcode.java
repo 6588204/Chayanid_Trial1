20fb1a13c2e0afd611e410db5aeb3bc6eb7f9675
6588204 <109618305+6588204@users.noreply.github.com>
2022-07-21 17:13:08 +0700

    Update myfirstcode.java

--- myfirstcode.java
+++ myfirstcode.java
@@ -1,2 +1,7 @@
-This is my first code is Java.
+// This is my first code is Java.
+  class Helloworld {
+    puplic static void main(String[] args) {
+      System.out.println("Hello World!");
+    }
+ }
 
